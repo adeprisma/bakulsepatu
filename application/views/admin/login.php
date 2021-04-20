eb6687647a5e921044d144bf4620279f452ef33d
Ade Prisma <adeprisma7@gmail.com>
2021-04-20 08:31:05 +0700

    login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login Page</title>
+        <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/img/favicon.ico') ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?= $title ?></title>
         <link href="<?= base_url('assets/admin/css/bootstrap.min.css') ?>" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="<?= base_url('assets/admin/bootstrap-select-1.12.1/bootstrap-select.min.css') ?>">
