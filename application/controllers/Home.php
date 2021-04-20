a782ba89cfca08c80a48cb23dffafe0bc7137a44
Ade Prisma <adeprisma7@gmail.com>
2021-04-20 15:33:09 +0700

    logo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,10 +12,11 @@ class Home extends MY_Controller
 	public function index()
 	{
 		$url = base_url();
+		$img = base_url('assets/img/logo-bakulsepatu.png');
 		$data['logo'] = '<div class="logo">
 							<h1>
 								<a href='.$url.'>
-									<img class="navbar-brand-beranda" src="https://trekkersshoes.com/assets/img/logo.png">
+									<img class="navbar-brand-beranda" src='.$img.'>
 								</a>
 							</h1>
 						</div>';
